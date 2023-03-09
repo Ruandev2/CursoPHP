cb5fab8d4a6b2c1b323df9beba132ed49d54e026
Ruan Rodrigues <rupriwell123@outlook.com>
2023-03-09 09:42:42 -0300

    first commit

---
+++ ex00/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Melivarndo Maldiçao</title>
+</head>
+<body>
+    <h1>
+        <?php
+           print'Ola, Mundo!🌏' 
+        ?>
+    </h1>
+    <p>Vamos tentar nos livrar maldicao</p>
+</body>
+</html>
