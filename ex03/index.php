6868da0f4d8c5d5aec83ec9f221d2286901e2212
Ruan Rodrigues <rupriwell123@outlook.com>
2023-03-09 13:57:57 -0300

    Tipos Primitivos

---
+++ ex03/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos Primitivos PHP</title>
+</head>
+<body>
+    <h1>Teste Tipos Primitimos</h1>
+    <?php
+        //OX =HEXADECIMAL OB = BINARIO O = OCTAL 
+        //$num = 15;
+        //echo "O valor da variavel é $num"
+        
+        //$v = 15.5;
+        //var_dump($v);
+
+        //$num = 3e2;
+        //echo " O valor é  $num";
+        //var_dump($num);
+
+        //$num = (int)"950";//converti para inteiro
+        //var_dump($num);
+
+       // $casado = true;//valor Verdadeiro ou falco
+        //var_dump($casado);
+
+        //$vet = [6,2,9,3,5];//vetor 
+        //var_dump($vet);
+
+        //class Pessoa {
+          //  private string $name;
+        //}
+        //$p = new Pessoa;
+        //var_dump($p);
+    ?>
+</body>
+</html>
